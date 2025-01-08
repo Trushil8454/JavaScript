a7cee6f5d264841e79b2ff82fdcf8ec9881c1480
Trushil8454 <trushilpatel8454@gmail.com>
2025-01-08 08:55:40 +0000

    dom complete

--- testing.js
+++ testing.js
@@ -1,1 +1,28 @@
-console.log("testing")
+// console.log("stat")
+
+// setTimeout(function(){
+//     console.log("i am in asnyc")
+// },3000)
+
+// console.log("end")
+
+function loadscript(src,callback){
+    var script = document.createElement("script");
+    script.src = src;
+
+    script.onload = function(){
+        console.log("load script with src"+src)
+        callback(src);
+    }
+
+    document.body.appendchild(script);
+}
+
+function hello(src){
+    console.log("say hello" + src);
+}
+function goodmorning(src){
+    console.log("good morning"+src);
+}
+
+loadscript("https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js", hello)
